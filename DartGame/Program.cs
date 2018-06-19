e3c3302acc988b9e3704c8c3d957af7bccf921a1
Monica Björk <m.bjork95@gmail.com>
2018-06-19 18:54:58 +0200

    Just getting started

--- DartGame/Program.cs
+++ DartGame/Program.cs
@@ -13,33 +13,39 @@ namespace DartGame {
     class Game {
         List<Player> players = new List<Player>();
         
-        private void AddPlayer(string name) {
+        private void AddPlayer(string name) { // Todo: method to add new players by their name
 
         }
 
-        public void PlayGame() {
-
+        public void PlayGame() { // Todo: Welcome user, menu, add players, start game, etc.  
+            
         }
     }
 
     class Player {
         private string name = "no name";
 
-        int[] turns = new int[2];   // three elements, three throws.
+        int[] turns = new int[2];   // three elements for three throws.
 
-        private void CalculatePoints() {
+        private void CalculatePoints() { // Todo: Calculate all throws to get total number points.
 
         }
 
-        private void AddTurn(int firstThrow, int secondThrwo, int thirdThrow) {
+        private void AddTurn(int firstThrow, int secondThrwo, int thirdThrow) { // Todo: Add a round until someone wins. Player will write 3 integers or a computer will randomize
 
+        }
+
+        private string PrintTurns() { // Todo: Print the last round when finished.
+
+            int foo = 0;
+            return foo;
         }
     }
 
     class Turns {
         int throwOne, throwTwo, throwThree;
         
-        public int GetScore() {
+        public int GetScore() { // Todo: Return all points for a player.
 
             int foo = 0;
             return foo;
